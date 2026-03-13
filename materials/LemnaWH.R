e51bf5f0d20cf2bfa82a08f1b10f4cc38a0cb3e4
Marcelo Ponce <mponce0@users.noreply.github.com>
2026-03-13 01:03:50 -0400

    Create LemnaWH.R

---
+++ materials/LemnaWH.R
@@ -0,0 +1,6 @@
+PlantNumber <-c(106,84,125,111,109,126,4,11,12,11,7,8,23,21,28,31,27,21,32,45,43,47,32,44,17,9,28,31,24,21)
+
+Treatment <- c("Positive","Positive","Positive","Positive","Positive","Positive",
+“Negative","Negative","Negative","Negative","Negative","Negative",
+"NaCl","NaCl","NaCl","NaCl","NaCl","NaCl","KCl","KCl","KCl","KCl","KCl","KCl",
+"CaCl2","CaCl2","CaCl2","CaCl2","CaCl2","CaCl2")
